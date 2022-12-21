f83eb393eed4301c01d94d1a8e60cb83bf2b9d84
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-21 12:26:03 +0100

    Just a silly test
    (Written in pure Lua in order to prevent issues I had to cover before writing the compiler itself)

---
+++ Script/ScyndiCoreTest.lua
@@ -0,0 +1,13 @@
+--[[
+
+	This script only serves to test the core script
+	(All Scyndi translated scripts need this script
+	as a basis for their own opersations)
+
+]]
+
+require "ScyndiCore" 
+
+Scyndi.Globals.Print("Hello World!")
+Scyndi.Globals.Cout("Hallo Welt!",Scyndi.Globals.endl)
+Scyndi.Globals.Cout("Salut ","monde!","\n")
