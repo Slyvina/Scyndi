18e1916875f3660fdfad0d493b0421f834765e14
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-29 16:16:57 +0100

    Update Translate.cpp

--- Compiler/Translate.cpp
+++ Compiler/Translate.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 22.12.27
+// Version: 22.12.29
 // EndLic
 
 #include <Slyvina.hpp>
