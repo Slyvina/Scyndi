573c461e1ec9073cfde7c6d72d79476fbe54a9cd
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-12-30 10:56:08 +0100

    vUpdate

--- Script/ScyndiCore.lua
+++ Script/ScyndiCore.lua
@@ -1,7 +1,7 @@
 -- <License Block>
 -- Script/ScyndiCore.lua
 -- Scyndi - Core Script
--- version: 22.12.27
+-- version: 22.12.30
 -- Copyright (C) 2022 Jeroen P. Broks
 -- This software is provided 'as-is', without any express or implied
 -- warranty.  In no event will the authors be held liable for any damages
