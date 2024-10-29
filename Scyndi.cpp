467f45a51e6ac450356ccf639fce0e973a22e540
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-29 14:17:08 +0100

    Code Rot!

--- Scyndi.cpp
+++ Scyndi.cpp
@@ -1,3 +1,29 @@
+// License:
+// 
+// Scyndi
+// Main File
+// 
+// 
+// 
+// 	(c) Jeroen P. Broks, 2022, 2023, 2024
+// 
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 	Please note that some references to data like pictures or audio, do not automatically
+// 	fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 24.10.29
+// End License
 // Lic:
 // Scyndi
 // Main File
@@ -49,7 +75,7 @@ int main(int nargs, char** args) {
 	QCol->Doing("Version", QVersion.Version(true));
 	QCol->Doing("Coded by", "Jeroen P. Broks");
 	std::cout << "\n";
-	QCol->Doing("Lua version", Slyvina::Lunatic::_Lunatic::Lua_Version());
+	QCol->Doing("Lua version", Slyvina::NSLunatic::_Lunatic::Lua_Version());
 	QCol->Doing("Lua developed by", "PUC Rio");
 	std::cout << "\n\n\n";
 	FlagConfig cargs{};
