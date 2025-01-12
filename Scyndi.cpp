35b9f4af6b371629124f78d6d45daff947336bc8
Jeroen P. Broks <jeroen.tricky@gmail.com>
2025-01-12 01:33:24 +0100

    Platform notice

--- Scyndi.cpp
+++ Scyndi.cpp
@@ -5,7 +5,7 @@
 // 
 // 
 // 
-// 	(c) Jeroen P. Broks, 2022, 2023, 2024
+// 	(c) Jeroen P. Broks, 2022, 2023, 2024, 2025
 // 
 // 		This program is free software: you can redistribute it and/or modify
 // 		it under the terms of the GNU General Public License as published by
@@ -22,33 +22,8 @@
 // 	Please note that some references to data like pictures or audio, do not automatically
 // 	fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 24.11.18
+// Version: 25.01.12
 // End License
-// Lic:
-// Scyndi
-// Main File
-// 
-// 
-// 
-// (c) Jeroen P. Broks, 2022, 2023
-// 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
-// Please note that some references to data like pictures or audio, do not automatically
-// fall under this licenses. Mostly this is noted in the respective files.
-// 
-// Version: 23.11.01
-// EndLic
 
 #include <Lunatic.hpp>
 #include <SlyvQCol.hpp>
@@ -58,7 +33,7 @@
 #include <JCR6_RealDir.hpp>
 #include <JCR6_zlib.hpp>
 
-#include "ScyndiVersion.hpp" 
+#include "ScyndiVersion.hpp"
 #include "Compiler/Config.hpp"
 #include "Compiler/ScyndiProject.hpp"
 
@@ -73,6 +48,7 @@ int main(int nargs, char** args) {
 	JCR6_InitRealDir();
 	QCol->LGreen("Scyndi Compiler\n");
 	QCol->Doing("Version", QVersion.Version(true));
+	QCol->Doing("Platform",Platform());
 	QCol->Doing("Coded by", "Jeroen P. Broks");
 	std::cout << "\n";
 	QCol->Doing("Lua version", Slyvina::NSLunatic::_Lunatic::LuaVersion());
